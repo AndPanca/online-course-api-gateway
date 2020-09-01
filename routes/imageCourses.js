0d6dcc5dc2de95bf5de2c7531e38d774f2628bda
AndPanca <andripanca11@gmail.com>
2020-09-02 05:46:53 +0700

    edit command imageCoursesHandler

--- routes/imageCourses.js
+++ routes/imageCourses.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const imageCoursesHandler = require('./handler/image-courses');
 
+// Router Image Courses file with imageCoursesHandler
 router.post('/', imageCoursesHandler.create);
 router.delete('/:id', imageCoursesHandler.destroy);
 
